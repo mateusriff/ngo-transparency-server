672372f093e3e586a178ea58918f5c0fcf253e6d
Pedro Stelzer <stelzerpedro2@gmail.com>
2025-02-27 21:33:11 -0300

    refactor: add logs for rquest start and execution in routes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlmodel import Session, select
 from models import Post, PostCreate, PostPatch
 import logging
+import time
 
 app = FastAPI()
 
@@ -26,10 +27,19 @@ def create_post(post: PostCreate, session: SessionDep):
 
     try:
 
+        logger.info("Request initiated: POST /posts/")
+        start_time = time.perf_counter()
+
         new_post = Post.model_validate(post)
         session.add(new_post)
         session.commit()
         session.refresh(new_post)
+
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+
+        logger.info("Request finalized successfully: POST /posts/")
+        logger.info(f"Execution time: {elapsed_time:.6f} seconds")
 
         return new_post
     
@@ -43,11 +53,20 @@ def read_posts(ong_id: int, session: Session = Depends(get_session)):
 
     try:
 
+        logger.info(f"Request initiated: GET /posts/{ong_id}")
+        start_time = time.perf_counter()
+
         logger.info(f"Fetching posts for ONG ID: {ong_id}")                 
         posts = session.exec(select(Post).where(Post.ong == ong_id)).all()
 
         if not posts:
             raise HTTPException(status_code = 404, detail = "Posts not found")
+        
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+
+        logger.info(f"Request finalized successfully: GET /posts/{ong_id}")
+        logger.info(f"Execution time: {elapsed_time:.6f} seconds")
         
         return posts
     
@@ -61,6 +80,9 @@ def update_post(ong_id: int, id: int, post: PostPatch, session: Session = Depend
 
     try:
 
+        logger.info(f"Request initiated: PATCH /posts/{ong_id}/{id}")
+        start_time = time.perf_counter()
+
         db_post = session.get(Post, id)
 
         if not db_post or db_post.ong != ong_id:
@@ -72,6 +94,12 @@ def update_post(ong_id: int, id: int, post: PostPatch, session: Session = Depend
         session.add(db_post)
         session.commit()
         session.refresh(db_post)
+
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+
+        logger.info(f"Request finalized successfully: PATCH /posts/{ong_id}/{id}")
+        logger.info(f"Execution time: {elapsed_time:.6f} seconds")
 
         return db_post
     
@@ -85,6 +113,9 @@ def delete_post(ong_id: int, id: int, session: Session = Depends(get_session)):
 
     try:
 
+        logger.info(f"Request initiated: DELETE /posts/{ong_id}/{id}")
+        start_time = time.perf_counter()
+
         db_post = session.get(Post, id)
 
         if not db_post or db_post.ong != ong_id:
@@ -92,6 +123,12 @@ def delete_post(ong_id: int, id: int, session: Session = Depends(get_session)):
         
         session.delete(db_post)
         session.commit()
+
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+
+        logger.info(f"Request finalized successfully: DELETE /posts/{ong_id}/{id}")
+        logger.info(f"Execution time: {elapsed_time:.6f} seconds")
 
         return {"detail": "Post deleted"}
     
