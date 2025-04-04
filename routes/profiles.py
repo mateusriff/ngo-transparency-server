f7ba26aa132b5c845e586820ef8efdbe145c52b7
Pedro Stelzer <stelzerpedro2@gmail.com>
2025-04-03 23:57:44 -0300

    bugfix: profiles get response not return a list anymore

--- routes/profiles.py
+++ routes/profiles.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 @router.post("/")
 def create_profile(profile: Profile, session: SessionDep):
     try:
-        logger.info("Request initiated: POST /profiles/")
+        logger.info("Request initiated: POST /profile/")
         start_time = time.perf_counter()
 
         new_profile = profile
@@ -24,7 +24,7 @@ def create_profile(profile: Profile, session: SessionDep):
         end_time = time.perf_counter()
         elapsed_time = end_time - start_time
 
-        logger.info("Request finalized successfully: POST /profiles/")
+        logger.info("Request finalized successfully: POST /profile/")
         logger.info(f"Execution time: {elapsed_time:.6f} seconds")
 
         return new_profile
@@ -36,31 +36,31 @@ def create_profile(profile: Profile, session: SessionDep):
 @router.get("/{ong_id}")
 def read_profiles(ong_id: int, session: Session = Depends(get_session)):
     try:
-        logger.info(f"Request initiated: GET /profiles/{ong_id}")
+        logger.info(f"Request initiated: GET /profile/{ong_id}")
         start_time = time.perf_counter()
 
-        logger.info(f"Fetching profiles for ONG ID: {ong_id}")
-        profiles = session.exec(select(Profile).where(Profile.ong == ong_id)).all()
+        logger.info(f"Fetching profile for ONG ID: {ong_id}")
+        profile = session.exec(select(Profile).where(Profile.ong == ong_id)).first()
 
-        if not profiles:
-            raise HTTPException(status_code=404, detail="Profiles not found")
+        if not profile:
+            raise HTTPException(status_code=404, detail="Profile not found")
 
         end_time = time.perf_counter()
         elapsed_time = end_time - start_time
 
-        logger.info(f"Request finalized successfully: GET /profiles/{ong_id}")
+        logger.info(f"Request finalized successfully: GET /profile/{ong_id}")
         logger.info(f"Execution time: {elapsed_time:.6f} seconds")
 
-        return profiles
+        return profile
 
     except Exception as e:
-        logger.error(f"Error fetching profiles: {e}")
+        logger.error(f"Error fetching profile: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 @router.delete("/{ong_id}/{id}")
 def delete_profile(ong_id: int, id: int, session: Session = Depends(get_session)):
     try:
-        logger.info(f"Request initiated: DELETE /profiles/{ong_id}/{id}")
+        logger.info(f"Request initiated: DELETE /profile/{ong_id}/{id}")
         start_time = time.perf_counter()
 
         db_profile = session.get(Profile, id)
@@ -74,7 +74,7 @@ def delete_profile(ong_id: int, id: int, session: Session = Depends(get_session)
         end_time = time.perf_counter()
         elapsed_time = end_time - start_time
 
-        logger.info(f"Request finalized successfully: DELETE /profiles/{ong_id}/{id}")
+        logger.info(f"Request finalized successfully: DELETE /profile/{ong_id}/{id}")
         logger.info(f"Execution time: {elapsed_time:.6f} seconds")
 
         return {"detail": "Profile deleted"}
